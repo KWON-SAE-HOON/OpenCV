a7fce696213c9f6f66ed0f675307ff731ddee56a
KWON-SAE-HOON <sts1051@gmail.com>
2023-03-30 17:46:16 +0900

    커밋4 - 그림파일 밝게하기

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ scr = cv2.imread('images/picture01.jpg')
 
 dst1 = cv2.cvtColor(scr, cv2.COLOR_RGB2GRAY)
 dst2 = scr.copy()
-_ , dst2 = cv2.threshold(dst2, 127, 255, cv2.THRESH_BINARY)
+dst3 = cv2.add(src, (100, 100, 100, 0))
 
 
 
@@ -17,6 +17,7 @@ _ , dst2 = cv2.threshold(dst2, 127, 255, cv2.THRESH_BINARY)
 cv2.imshow('scr', scr)
 cv2.imshow('dst1', dst1)
 cv2.imshow('dst2', dst2)
+cv2.imshow('dst3', dst3)
 
 
 
